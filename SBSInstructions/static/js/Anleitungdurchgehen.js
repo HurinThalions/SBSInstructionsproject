47f2025326456bb926e7cfe05c48930bf6d0a532
ozanaksu123 <ozziaksu99@gmail.com>
2023-05-24 13:00:24 +0200

    Javascript Testschritt1

--- SBSInstructions/static/js/Anleitungdurchgehen.js
+++ SBSInstructions/static/js/Anleitungdurchgehen.js
@@ -1,42 +1,63 @@
-
 function loadAnleitungData1() {
+    let elements = document.querySelectorAll('li');
+    let container = document.getElementById('schritt-details');
+    let jsondata = JSON.parse(document.getElementById('schritte-json').textContent);
 
-    let elements = document.querySelectorAll('li')
-    let container = document.getElementById('schritt-details')
+    let firstElement = elements[0];
+    let firstIndex = firstElement.dataset.index;
+    let firstData = jsondata[firstIndex];
 
-    // Holt die daten aus dem html und speichert sie in jsondata
-    let jsondata = JSON.parse(document.getElementById('schritte-json').textContent)
-    console.log(jsondata);
-
+    addEinzelschritt(container, firstData);
+    addKomponenten(container, firstData);
 
     elements.forEach((element) => {
         element.addEventListener('click', function() {
-            removeContainerChildren(container)
-            addEinzelschritt(container, element)
-        })
-    })
+            removeContainerChildren(container);
+            let index = element.dataset.index;
+            let data = jsondata[index];
+            addEinzelschritt(container, data);
+            addKomponenten(container, data);
+        });
+    });
 
-    function addEinzelschritt(container, schrittElement) {
+    function addEinzelschritt(container, data) {
+        let einzelschrittElement = document.createElement('div');
+        einzelschrittElement.classList.add('einzelschritt');
 
-        let index = schrittElement.dataset.index
-        let data = jsondata[index]
-        console.log(data)
+        let schrittbenennungElement = document.createElement('h1');
+        let benennungText = document.createTextNode(data.schrittbenennung);
+        schrittbenennungElement.appendChild(benennungText);
 
-        let schrittbenennungElement = document.createElement('h1')
-        let benunngText = document.createTextNode(data.schrittbenennung)
-        schrittbenennungElement.appendChild(benunngText)
+        let beschreibungElement = document.createElement('p');
+        let beschreibungText = document.createTextNode(data.beschreibung);
+        beschreibungElement.appendChild(beschreibungText);
 
-        let schrittbeschreibungElement = document.createElement('p')
-        let beschreibungText = document.createTextNode(data.beschreibung)
-        schrittbeschreibungElement.appendChild(beschreibungText)
+        einzelschrittElement.appendChild(schrittbenennungElement);
+        einzelschrittElement.appendChild(beschreibungElement);
 
-        container.append(schrittbenennungElement, schrittbeschreibungElement)
+        container.innerHTML = '';
+        container.appendChild(einzelschrittElement);
+    }
+
+    function addKomponenten(container, data) {
+        let komponentenElement = document.createElement('div');
+        komponentenElement.classList.add('komponenten');
+
+        data.komponenten.forEach((komponente) => {
+            let komponenteElement = document.createElement('div');
+            komponenteElement.classList.add('komponente');
+            komponenteElement.textContent = komponente;
+            komponentenElement.appendChild(komponenteElement);
+        });
+
+        container.appendChild(komponentenElement);
     }
 
     function removeContainerChildren(container) {
         while (container.firstChild) {
-            container.removeChild(container.firstChild)
+            container.removeChild(container.firstChild);
         }
     }
-
 }
+
+loadAnleitungData1();
