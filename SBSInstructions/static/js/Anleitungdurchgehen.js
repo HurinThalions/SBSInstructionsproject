6497b2c5fbd01b69b831b8f4f586e7e5a54fea9e
ozanaksu123 <ozziaksu99@gmail.com>
2023-06-02 18:03:59 +0200

    Anleitungdurchgehen: Beschreibung und Komponenten

--- SBSInstructions/static/js/Anleitungdurchgehen.js
+++ SBSInstructions/static/js/Anleitungdurchgehen.js
@@ -25,10 +25,9 @@ function loadAnleitungData1() {
   addKomponenten(container, firstData);
 
   // Alle Elemente werden geladen und angezeigt
-  // Diesen teil verstehe ich noch nicht ganz. Doppelung mit dem Codeteil darüber?
   elements.forEach((element) => {
-      let index = element.dataset.index;
-      let data = jsondata[index];
+    let index = element.dataset.index;
+    let data = jsondata[index];
     element.addEventListener("load", function () {
       removeContainerChildren(container);
       addEinzelschritt(container, data);
@@ -42,14 +41,24 @@ function loadAnleitungData1() {
     einzelschrittElement.classList.add("einzelschritt");
 
     let schrittbenennungElement = document.createElement("h1");
-    let benennungText = document.createTextNode(data.schrittbenennung);
+    let benennungText = document.createTextNode("Schritt " + counter + ": " + data.schrittbenennung);
     schrittbenennungElement.appendChild(benennungText);
     schrittbenennungElement.classList.add("schrittbenennung");
+
+    let beschreibungUberschriftElement = document.createElement("h2");
+    let beschreibungUberschriftText = document.createTextNode("Beschreibung:");
+    beschreibungUberschriftElement.appendChild(beschreibungUberschriftText);
+
+    let beschreibungBoxElement = document.createElement("div");
+    beschreibungBoxElement.classList.add("beschreibung-box");
 
     let beschreibungElement = document.createElement("p");
     let beschreibungText = document.createTextNode(data.beschreibung);
     beschreibungElement.appendChild(beschreibungText);
     beschreibungElement.classList.add("beschreibung");
+
+    beschreibungBoxElement.appendChild(beschreibungUberschriftElement);
+    beschreibungBoxElement.appendChild(beschreibungElement);
 
     let schrittBildElement = document.createElement("img");
     schrittBildElement.setAttribute("src", "/media/" + data.schrittbild);
@@ -57,7 +66,7 @@ function loadAnleitungData1() {
     console.log(data.schrittbild);
 
     einzelschrittElement.appendChild(schrittbenennungElement);
-    einzelschrittElement.appendChild(beschreibungElement);
+    einzelschrittElement.appendChild(beschreibungBoxElement);
     einzelschrittElement.appendChild(schrittBildElement);
 
     container.innerHTML = "";
@@ -65,41 +74,46 @@ function loadAnleitungData1() {
   }
 
   // Komponenten, die zum aktuellen Schritt gehören, werden geholt und auf der Seite geladen
-function addKomponenten(container, data) {
-  // ID des aktuellen Anleitungsschritts
-  let anleitungsschrittId = data.id;
-  let komponentenContainer = document.createElement("div");
+  function addKomponenten(container, data) {
+    // ID des aktuellen Anleitungsschritts
+    let anleitungsschrittId = data.id;
+    let komponentenContainer = document.createElement("div");
 
-  // Leeren des Containers bevor neue Komponenten hinzugefügt werden
-  komponentenContainer.innerHTML = "";
+    // Leeren des Containers bevor neue Komponenten hinzugefügt werden
+    komponentenContainer.innerHTML = "";
 
-  // Komponenten werden anhand des ForeignKeys gefiltert
-  let komponenten = kompdata.filter(
-    (komponente) => komponente.anleitungsschritt_id === anleitungsschrittId
-  );
+    // Komponenten werden anhand des ForeignKeys gefiltert
+    let komponenten = kompdata.filter(
+      (komponente) => komponente.anleitungsschritt_id === anleitungsschrittId
+    );
 
-  // Komponenten werden auf die Seite geladen
-  komponenten.forEach((komponente) => {
-    let komponenteElement = document.createElement("div");
-    komponenteElement.classList.add("komponente");
+    // Komponenten werden auf die Seite geladen
+    if (komponenten.length > 0) {
+      let komponentenUberschriftElement = document.createElement("h2");
+      let komponentenUberschriftText = document.createTextNode("Komponenten:");
+      komponentenUberschriftElement.appendChild(komponentenUberschriftText);
+      komponentenContainer.appendChild(komponentenUberschriftElement);
 
-    let komponenteBeschreibung = document.createElement("p");
-    komponenteBeschreibung.textContent = komponente.kompbeschreibung;
-    komponenteBeschreibung.classList.add("kompbeschreibung");
-    komponenteElement.appendChild(komponenteBeschreibung);
+      komponenten.forEach((komponente) => {
+        let komponenteElement = document.createElement("div");
+        komponenteElement.classList.add("komponente");
 
-    let komponenteBildElement = document.createElement("img");
-    komponenteBildElement.setAttribute("src", "/media/" + komponente.kompbild);
-    komponenteBildElement.classList.add("kompbild");
-    
-    komponenteElement.appendChild(komponenteBildElement);
-    komponentenContainer.appendChild(komponenteElement);
-  });
+        let komponenteBeschreibung = document.createElement("p");
+        komponenteBeschreibung.textContent = komponente.kompbeschreibung;
+        komponenteBeschreibung.classList.add("kompbeschreibung");
+        komponenteElement.appendChild(komponenteBeschreibung);
 
-  container.appendChild(komponentenContainer);
-}
+        let komponenteBildElement = document.createElement("img");
+        komponenteBildElement.setAttribute("src", "/media/" + komponente.kompbild);
+        komponenteBildElement.classList.add("kompbild");
 
-  
+        komponenteElement.appendChild(komponenteBildElement);
+        komponentenContainer.appendChild(komponenteElement);
+      });
+    }
+
+    container.appendChild(komponentenContainer);
+  }
 
   // Entfernen des Anleitungsschritts und der Komponenten
   function removeContainerChildren(container) {
@@ -136,10 +150,9 @@ function addKomponenten(container, data) {
   function previousAnleitungsschritt() {
     counter--;
     if (counter < 0) {
-          
-          // Popup für Anleitung Abbrechen? -> Ja/Nein
-          // wenn ja -> window.location.pathname = "SBSInstructionsproject/Startbildschirm";
-          // wenn nein -> counter = 0;
+      // Popup für Anleitung Abbrechen? -> Ja/Nein
+      // wenn ja -> window.location.pathname = "SBSInstructionsproject/Startbildschirm";
+      // wenn nein -> counter = 0;
     }
     let previousElement = elements[counter];
     let previousIndex = previousElement.dataset.index;
