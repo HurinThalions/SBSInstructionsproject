91bf6b8182571ae8b521e9cf11993c54a3adb5a1
ozanaksu123 <ozziaksu99@gmail.com>
2023-06-04 15:41:00 +0200

    Design verbesserungen

--- SBSInstructions/static/js/AD.js
+++ SBSInstructions/static/js/AD.js
@@ -13,12 +13,6 @@ function popupOnV()
     document.getElementById("overlayVollbild").style.display = "block";
 }
 
-// function popupOnV() {
-//     var imageURL = document.getElementById("schrittbildrechtezelle").src;
-//     document.documentElement.style.setProperty('--bg-image', 'url("' + imageURL + '")');
-//     document.getElementById("overlayVollbild").style.display = "block";
-// }
-
 function popupOffV()
 {
     document.getElementById("overlayVollbild").style.display = "none";
